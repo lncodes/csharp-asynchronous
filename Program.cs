213224409fdd7f8c16d28b50e3b14cce7d49d862
Theodorus Doni <theodorusdoni4@gmail.com>
2021-03-26 08:25:34 +0700

    Migrate project to github repo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lncodes.Example.Async
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            //Create TimerController Instance
+            var timerController = new TimerController(delay: 1000, duration: 10000);
+            Console.WriteLine("!!! PRESS ENTER TO STOP THE TIMER !!!");
+            await timerController.StartAsync();
+        }
+    }
+}
