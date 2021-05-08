0fc845986bd88bee21483e34ec5fb014e1e1599b
Theodorus Doni <theodorusdoni4@gmail.com>
2021-05-08 21:22:53 +0700

    Fix some code coverage isssue

--- src/Program.cs
+++ src/Program.cs
@@ -14,7 +14,7 @@ namespace Lncodes.Example.Async
         /// Main Program
         /// </summary>
         /// <returns cref=Task></returns>
-        static async Task Main()
+        private static async Task Main()
         {
             var timerController = new TimerController(delay: 1000, duration: 10000);
             Console.WriteLine("!!! PRESS ENTER TO STOP THE TIMER !!!");
